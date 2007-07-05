022e3afde460462c7790aa7e06e7e3991719bb07
Mike Frysinger <vapier@gentoo.org>
2007-07-05 21:13:12 +0000

    improve patch version handling

--- make-tarball.sh
+++ make-tarball.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]] ; then
-	echo "Usage: $0 <glibc ver> <patch ver>"
-	exit 1
-fi
 gver=$1
 pver=$2
 
+if [[ -z ${gver} ]] ; then
+	echo "Usage: $0 <glibc ver> [patch ver]"
+	exit 1
+fi
+
 if [[ ! -d ./${gver} ]] ; then
 	echo "Error: ${gver} is not a valid glibc ver"
+	exit 1
+fi
+
+if [[ -e ${gver}/README.history ]] ; then
+	auto_pver=$(head -n 1 ${gver}/README.history | awk '{print $1}')
+	if [[ -z ${pver} ]] ; then
+		pver=${auto_pver}
+		echo "Autoguessing patch ver as '${pver}' from ${gver}/README.history"
+	elif [[ ${pver} != ${auto_pver} ]] ; then
+		echo "Warning: user patch version (${pver}) does not match ${gver}/README.history"
+	fi
+elif [[ -z ${pver} ]] ; then
+	echo "Error: need a patch version"
 	exit 1
 fi
 
@@ -18,7 +32,7 @@ rm -f glibc-${gver}-*.tar.bz2
 mkdir -p tmp/patches
 cp -r ${gver}/*.patch ../README* tmp/patches/ || exit 1
 cp -r extra tmp/ || exit 1
-cp ${gver}/README* tmp/patches/ || exit 1
+cp ${gver}/README* tmp/patches/ 2>/dev/null
 
 if [[ -d ${gver}/man ]] ; then
 	mkdir tmp/man
