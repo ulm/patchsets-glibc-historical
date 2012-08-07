13b59d464e2d8e6e24b6fd7540e19d7b277ebb5f
Mike Frysinger <vapier@gentoo.org>
2012-08-07 20:15:55 +0000

    generalize script a bit with $PN/$PV variables

--- make-tarball.sh
+++ make-tarball.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-gver=${1%/}
+PN="glibc"
+PV=${1%/}
 pver=$2
 
-if [[ -z ${gver} ]] ; then
-	echo "Usage: $0 <glibc ver> [patch ver]"
+if [[ -z ${PV} ]] ; then
+	echo "Usage: $0 <${PN} ver> [patch ver]"
 	exit 1
 fi
 
-if [[ ! -d ./${gver} ]] ; then
-	echo "Error: ${gver} is not a valid glibc ver"
+if [[ ! -d ./${PV} ]] ; then
+	echo "Error: ${PV} is not a valid ${PN} ver"
 	exit 1
 fi
 
-if [[ -e ${gver}/README.history ]] ; then
-	auto_pver=$(head -n 1 ${gver}/README.history | awk '{print $1}')
+if [[ -e ${PV}/README.history ]] ; then
+	auto_pver=$(head -n 1 ${PV}/README.history | awk '{print $1}')
 	if [[ -z ${pver} ]] ; then
 		pver=${auto_pver}
-		echo "Autoguessing patch ver as '${pver}' from ${gver}/README.history"
+		echo "Autoguessing patch ver as '${pver}' from ${PV}/README.history"
 	elif [[ ${pver} != ${auto_pver} ]] ; then
-		echo "Warning: user patch version (${pver}) does not match ${gver}/README.history"
+		echo "Warning: user patch version (${pver}) does not match ${PV}/README.history"
 	fi
 elif [[ -z ${pver} ]] ; then
 	echo "Error: need a patch version"
@@ -27,24 +28,24 @@ elif [[ -z ${pver} ]] ; then
 fi
 
 rm -rf tmp
-rm -f glibc-${gver}-*.tar.bz2
+rm -f ${PN}-${PV}-*.tar.bz2
 
 mkdir -p tmp/patches
-cp -r ${gver}/*.patch ../README* tmp/patches/ || exit 1
+cp -r ${PV}/*.patch ../README* tmp/patches/ || exit 1
 cp -r extra tmp/ || exit 1
-cp ${gver}/README* tmp/patches/ 2>/dev/null
+cp ${PV}/README* tmp/patches/ 2>/dev/null
 
-if [[ -d ${gver}/man ]] ; then
+if [[ -d ${PV}/man ]] ; then
 	mkdir tmp/man
-	cp -r ${gver}/man/* tmp/man/ || exit 1
+	cp -r ${PV}/man/* tmp/man/ || exit 1
 fi
 
 find tmp -type d -name CVS -print0 | xargs -0 rm -r
 
-tar -jcf glibc-${gver}-patches-${pver}.tar.bz2 \
+tar -jcf ${PN}-${PV}-patches-${pver}.tar.bz2 \
 	-C tmp patches extra || exit 1
-if [[ -d ${gver}/man ]] ; then
-	tar -jcf glibc-${gver}-manpages.tar.bz2 \
+if [[ -d ${PV}/man ]] ; then
+	tar -jcf ${PN}-${PV}-manpages.tar.bz2 \
 		-C tmp man || exit 1
 fi
 rm -r tmp
